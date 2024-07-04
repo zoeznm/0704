7b9b9e833a570b3b830981294b45d0f515ac1ca5
zoeznm <bomikim046@gmail.com>
2024-07-04 11:34:37 +0900

    menu.js 로 변환

--- dist/menu.js
+++ dist/menu.js
@@ -5,8 +5,18 @@ var anchorTags = function anchorTags(href, textNode) {
   var result = "<a href=\"".concat(href, "\">").concat(textNode, "</a>");
   return result;
 };
+var basicData = {
+  bomi: "보미",
+  jaemin: "재민",
+  ujin: "유진",
+  jiyoon: "지윤"
+};
 var liTags = function liTags(children) {
   var result = "<li>".concat(children, "</li>");
   return result;
 };
-root.innerHTML = "\n<ul>\n  ".concat(liTags(anchorTags("#보미", "보미")), "\n</ul>");
+var totalElement = function totalElement(object) {
+  var result = liTags(anchorTags("".concat(object.bomi), object.bomi));
+  return result;
+};
+root.innerHTML = "\n<ul>\n  ".concat(totalElement(basicData), "\n</ul>");
