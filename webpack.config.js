e141abb4f2b4778e07b2d4acea7da6df15db28ed
zoeznm <bomikim046@gmail.com>
2024-07-04 12:48:40 +0900

    번들링 완료

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: "",
-  output: ""
-}
+  entry: './dist/menu.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'menu.bundle.js',
+  },
+};
